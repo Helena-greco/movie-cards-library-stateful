51a06d246e3964c7479e6dcae6d79e1edf2ea306
Helena <nenafgreco@hotmail.com>
2021-09-09 19:20:45 -0300

    Requisitos 15 ao 17 - somente definindo as props

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
-      <AddMovie />
       <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
